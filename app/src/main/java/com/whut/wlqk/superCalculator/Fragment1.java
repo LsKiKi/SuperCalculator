22d1b442dc9fd772b715ea7e5f37d88f1e1d5eac
poison-li <guili96@163.com>
2019-01-09 12:07:49 +0800

    First commit

---
+++ app/src/main/java/com/whut/wlqk/superCalculator/Fragment1.java
@@ -0,0 +1,260 @@
+package com.whut.wlqk.superCalculator;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+
+
+
+public class Fragment1 extends Fragment /*implements View.OnClickListener*/ {
+    public Fragment1() {
+        // Required empty public constructor
+    }
+
+
+    //显示框
+    EditText input;
+    //按钮对象
+    Button clear, delete, charAdd, charDiv, charMul, charSub, point, equal;
+    Button num0, num1, num2, num3, num4, num5, num6, num7, num8, num9;
+    boolean clr_flag;    //判断et中是否清空
+    boolean point_flag = false;
+
+    /*@Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment1, container, false);
+        ButterKnife.bind(this, view);
+        initData();
+        return view;
+    }
+
+    private void initData() {
+        //按钮对象
+        clear = (Button) findViewById(R.id.btn_AC);
+        delete = (Button) findViewById(R.id.btn_Del);
+        charAdd = (Button) findViewById(R.id.btn_add);
+        charSub = (Button) findViewById(R.id.btn_sub);
+        charMul = (Button) findViewById(R.id.btn_mul);
+        charDiv = (Button) findViewById(R.id.btn_div);
+        point = (Button) findViewById(R.id.btn_point);
+        equal = (Button) findViewById(R.id.btn_equ);
+
+        num0 = (Button) findViewById(R.id.btn_0);
+        num1 = (Button) findViewById(R.id.btn_1);
+        num2 = (Button) findViewById(R.id.btn_2);
+        num3 = (Button) findViewById(R.id.btn_3);
+        num4 = (Button) findViewById(R.id.btn_4);
+        num5 = (Button) findViewById(R.id.btn_5);
+        num6 = (Button) findViewById(R.id.btn_6);
+        num7 = (Button) findViewById(R.id.btn_7);
+        num8 = (Button) findViewById(R.id.btn_8);
+        num9 = (Button) findViewById(R.id.btn_9);
+        input = (EditText) findViewById(R.id.input);
+
+        //设置按钮的点击事件
+        num0.setOnClickListener(this);
+        num1.setOnClickListener(this);
+        num2.setOnClickListener(this);
+        num3.setOnClickListener(this);
+        num4.setOnClickListener(this);
+        num5.setOnClickListener(this);
+        num6.setOnClickListener(this);
+        num7.setOnClickListener(this);
+        num8.setOnClickListener(this);
+        num9.setOnClickListener(this);
+        clear.setOnClickListener(this);
+        delete.setOnClickListener(this);
+        charAdd.setOnClickListener(this);
+        charSub.setOnClickListener(this);
+        charMul.setOnClickListener(this);
+        charDiv.setOnClickListener(this);
+        point.setOnClickListener(this);
+        equal.setOnClickListener(this);
+    }
+
+
+    public void onClick(View view) {
+        String str = input.getText().toString();
+        switch (view.getId()) {
+            case R.id.btn_0:
+            case R.id.btn_1:
+            case R.id.btn_2:
+            case R.id.btn_3:
+            case R.id.btn_4:
+            case R.id.btn_5:
+            case R.id.btn_6:
+            case R.id.btn_7:
+            case R.id.btn_8:
+            case R.id.btn_9:
+                if (clr_flag) {
+                    clr_flag = false;
+                    str = "";
+                    input.setText("");
+                }
+                input.setText(str + ((Button) view).getText());
+                break;
+            case R.id.btn_point:
+                if (point_flag) break;
+                point_flag = true;
+                if (clr_flag) {
+                    clr_flag = false;
+                    str = "";
+                    input.setText("");
+                }
+                input.setText(str + ((Button) view).getText());
+                break;
+            case R.id.btn_AC:
+                if (clr_flag) {
+                    clr_flag = false;
+                }
+                str = "";
+                input.setText("");
+                point_flag = false;
+                break;
+            case R.id.btn_add:
+            case R.id.btn_sub:
+            case R.id.btn_mul:
+            case R.id.btn_div:
+                if (clr_flag) {
+                    clr_flag = false;
+                    str = "";
+                    input.setText("");
+                }
+                point_flag = false;
+                if(str.contains("+")||str.contains("-")||str.contains("×")||str.contains("÷")) {
+                    str=str.substring(0,str.indexOf(" "));
+                }
+                input.setText(str + ((Button) view).getText());
+                break;
+            case R.id.btn_Del:
+                if (clr_flag) {
+                    clr_flag = false;
+                    str = "";
+                    input.setText("");
+                } else if (str != null && !str.equals("")) {
+                    if (str.charAt(str.length() - 1) == '.') {
+                        point_flag = false;
+                    } else if (str.charAt(str.length() - 1) < '9' && '0' < str.charAt(str.length() - 1)) {
+                        if (str.length()>2 && str.charAt(str.length() - 1) > '9' && '0' > str.charAt(str.length() - 1))
+                        {
+                            point_flag = false;
+                            break;
+                        }
+                    } else {
+                        for (int i = str.length() - 2; i >= 0; i--) {
+                            if (str.charAt(i) == '.') {
+                                point_flag = true;
+                                break;
+                            }
+                            if (str.charAt(i) != '.' && str.charAt(str.length() - 1) > '9' && '0' > str.charAt(str.length() - 1)) {
+                                point_flag = false;
+                                break;
+                            }
+                        }
+                    }
+                    input.setText(str.substring(0, str.length() - 1));
+                }
+
+                break;
+            case R.id.btn_equ:
+                getResult();
+            default:
+        }
+    }
+
+    private void getResult() {
+        String exp = input.getText().toString();
+        if (exp == null || exp.equals("")) return;
+        //因为没有运算符所以不用运算
+        if (!exp.contains(" ")) {
+            return;
+        }
+        if (clr_flag) {
+            clr_flag = false;
+            return;
+        }
+        clr_flag = true;
+        //截取运算符前面的字符串
+        String s1 = exp.substring(0, exp.indexOf(" "));
+        //截取的运算符
+        String op = exp.substring(exp.indexOf(" ") + 1, exp.indexOf(" ") + 2);
+        //截取运算符后面的字符串
+        String s2 = exp.substring(exp.indexOf(" ") + 3);
+        double cnt = 0;
+        if (!s1.equals("") && !s2.equals("")) {
+            double d1 = Double.parseDouble(s1);
+            double d2 = Double.parseDouble(s2);
+            if (op.equals("+")) {
+                cnt = d1 + d2;
+            }
+            if (op.equals("-")) {
+                cnt = d1 - d2;
+            }
+            if (op.equals("×")) {
+                cnt = d1 * d2;
+            }
+            if (op.equals("÷")) {
+                if (d2 == 0) cnt = 0;
+                else cnt = d1 / d2;
+            }
+            if (!s1.contains(".") && !s2.contains(".") && !op.equals("÷")) {
+                int res = (int) cnt;
+                input.setText(res + "");
+            } else {
+                input.setText(cnt + "");
+            }
+        }
+        //s1不为空但s2为空
+        else if (!s1.equals("") && s2.equals("")) {
+            double d1 = Double.parseDouble(s1);
+            if (op.equals("+")) {
+                cnt = d1;
+            }
+            if (op.equals("-")) {
+                cnt = d1;
+            }
+            if (op.equals("×")) {
+                cnt = 0;
+            }
+            if (op.equals("÷")) {
+                cnt = 0;
+            }
+            if (!s1.contains(".")) {
+                int res = (int) cnt;
+                input.setText(res + "");
+            } else {
+                input.setText(cnt + "");
+            }
+        }
+        //s1是空但s2不是空
+        else if (s1.equals("") && !s2.equals("")) {
+            double d2 = Double.parseDouble(s2);
+            if (op.equals("+")) {
+                cnt = d2;
+            }
+            if (op.equals("-")) {
+                cnt = 0 - d2;
+            }
+            if (op.equals("×")) {
+                cnt = 0;
+            }
+            if (op.equals("÷")) {
+                cnt = 0;
+            }
+            if (!s2.contains(".")) {
+                int res = (int) cnt;
+                input.setText(res + "");
+            } else {
+                input.setText(cnt + "");
+            }
+        } else {
+            input.setText("");
+        }
+    }*/
+
+}
---
+++ app/src/main/java/com/whut/wlqk/superCalculator/MainActivity.java
@@ -0,0 +1,148 @@
+package com.whut.wlqk.superCalculator;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private TextView item_jisuan, item_fangdai, item_shuidai;
+    private ViewPager vp;
+    private Fragment1 oneFragment;
+    private Fragment2 twoFragment;
+    private Fragment3 threeFragment;
+    private List<Fragment> mFragmentList = new ArrayList<Fragment>();
+    private FragmentAdapter  mFragmentAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //设置这个这句话所在的Activity采用R.layout下的main布局文件进行布局
+        setContentView(R.layout.activity_main);
+        initViews();
+
+        mFragmentAdapter = new FragmentAdapter(this.getSupportFragmentManager(), mFragmentList);
+        vp.setOffscreenPageLimit(3);//ViewPager的缓存为4帧
+        vp.setAdapter(mFragmentAdapter);
+        vp.setCurrentItem(0);//初始设置ViewPager选中第一帧
+        item_jisuan.setTextColor(getResources().getColor(R.color.colorTitleSelected));
+
+
+        //ViewPager的监听事件
+        vp.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                /*此方法在页面被选中时调用*/
+                changeTextColor(position);
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+                /*此方法是在状态改变的时候调用，其中arg0这个参数有三种状态（0，1，2）。
+                arg0 ==1的时辰默示正在滑动，
+                arg0==2的时辰默示滑动完毕了，
+                arg0==0的时辰默示什么都没做。*/
+            }
+        });
+    }
+
+    /**
+     * 初始化布局View
+     */
+    private void initViews() {
+        item_jisuan = (TextView) findViewById(R.id.item_jisuan);
+        item_fangdai = (TextView) findViewById(R.id.item_fangdai);
+        item_shuidai = (TextView) findViewById(R.id.item_shuidai);
+
+        item_jisuan.setOnClickListener(this);
+        item_fangdai.setOnClickListener(this);
+        item_shuidai.setOnClickListener(this);
+
+        vp = (ViewPager) findViewById(R.id.mainViewPager);
+        oneFragment = new Fragment1();
+        twoFragment = new Fragment2();
+        threeFragment = new Fragment3();
+
+        //给FragmentList添加数据
+        mFragmentList.add(oneFragment);
+        mFragmentList.add(twoFragment);
+        mFragmentList.add(threeFragment);
+    }
+
+
+    /**
+     * 点击顶部Text 动态修改ViewPager的内容
+     */
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.item_jisuan:
+                vp.setCurrentItem(0, true);
+                break;
+            case R.id.item_fangdai:
+                vp.setCurrentItem(1, true);
+                break;
+            case R.id.item_shuidai:
+                vp.setCurrentItem(2, true);
+                break;
+        }
+    }
+
+
+    /*
+     *由ViewPager的滑动修改顶部导航Text的颜色
+     */
+    private void changeTextColor(int position) {
+        if (position == 0) {
+            item_jisuan.setTextColor(getResources().getColor(R.color.colorTitleSelected));
+            item_fangdai.setTextColor(getResources().getColor(R.color.colorTitleUnSelected));
+            item_shuidai.setTextColor(getResources().getColor(R.color.colorTitleUnSelected));
+        } else if (position == 1) {
+            item_fangdai.setTextColor(getResources().getColor(R.color.colorTitleSelected));
+            item_jisuan.setTextColor(getResources().getColor(R.color.colorTitleUnSelected));
+            item_shuidai.setTextColor(getResources().getColor(R.color.colorTitleUnSelected));
+        } else if (position == 2) {
+            item_shuidai.setTextColor(getResources().getColor(R.color.colorTitleSelected));
+            item_jisuan.setTextColor(getResources().getColor(R.color.colorTitleUnSelected));
+            item_fangdai.setTextColor(getResources().getColor(R.color.colorTitleUnSelected));
+        }
+    }
+
+    public class FragmentAdapter extends FragmentPagerAdapter {
+
+        List<Fragment> fragmentList = new ArrayList<Fragment>();
+
+        public FragmentAdapter(FragmentManager fm, List<Fragment> fragmentList) {
+            super(fm);
+            this.fragmentList = fragmentList;
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return fragmentList.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return fragmentList.size();
+        }
+
+    }
+
+
+
+}
