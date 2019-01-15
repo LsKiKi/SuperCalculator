85de0355d08501b20b9ab2edc9875c6f38649a03
poison-li <guili96@163.com>
2019-01-15 11:49:10 +0800

    个税输入框输入闲置调整

--- app/src/main/java/com/whut/wlqk/superCalculator/annualBonus.java
+++ app/src/main/java/com/whut/wlqk/superCalculator/annualBonus.java
@@ -3,18 +3,19 @@ package com.whut.wlqk.superCalculator;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
 
-public class annualBonus  extends Fragment implements View.OnClickListener{
+public class annualBonus extends Fragment implements View.OnClickListener {
     EditText annualbonus;
-    double annualbonus_;
     Button btn;
+
     public annualBonus() {
         // Required empty public constructor
     }
@@ -26,22 +27,45 @@ public class annualBonus  extends Fragment implements View.OnClickListener{
         init(view);
         return view;
     }
-    private void init(View view){
-        annualbonus = view.findViewById(R.id.business_total_loan2);
+
+    private void init(View view) {
+        annualbonus = view.findViewById(R.id.total_annual);
         btn = view.findViewById(R.id.btn_start_compute);
         btn.setOnClickListener(this);
+        annualbonus.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                String str = s.toString();
+
+                /*
+                 * 0开始的整数
+                 */
+                if (str.startsWith("0") && !str.equals("0")) {
+                    s.delete(0, 1);
+                }
+            }
+        });
     }
 
     @Override
     public void onClick(View v) {
         try {
-            annualbonus_ = Double.parseDouble(annualbonus.getText().toString());
             Intent intent = new Intent(getActivity(), AnnualActivity.class);
             Bundle bundle = new Bundle();
-            bundle.putDouble("annualbonus", annualbonus_);
+            bundle.putDouble("annualbonus", Double.parseDouble(annualbonus.getText().toString()));
             intent.putExtras(bundle);
             startActivity(intent);
-        }catch (Exception ex){
+        } catch (Exception ex) {
             ex.printStackTrace();
             Toast.makeText(v.getContext(), getString(R.string.toast_error), Toast.LENGTH_SHORT).show();
         }
--- app/src/main/java/com/whut/wlqk/superCalculator/monthlySalary.java
+++ app/src/main/java/com/whut/wlqk/superCalculator/monthlySalary.java
@@ -3,6 +3,8 @@ package com.whut.wlqk.superCalculator;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -21,16 +23,16 @@ import com.whut.wlqk.superCalculator.utils.tax.Wuxianyijin;
 import java.text.DecimalFormat;
 import java.util.List;
 
-public class monthlySalary extends Fragment implements View.OnClickListener {
+public class monthlySalary extends Fragment {
 
     TextView insurance_housing_fund;
     LinearLayout layout;
     Spinner spinner_city;
-    String city;
     EditText input_salary, et_threshold, et_fund, et_medical, et_endowment, et_unemployment, et_employment_injury, et_maternity;
     boolean inf_visible = false;
     DbHelper dbHelper = null;
     Button compute;
+    Wuxianyijin wxyj;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -42,10 +44,119 @@ public class monthlySalary extends Fragment implements View.OnClickListener {
         return view;
     }
 
-    private void init(View view) {
+    private void init(final View view) {
+
+        /*
+         * 查找绑定资源
+         */
+        bind_view(view);
+
+        /*
+         * 从xml中填充数据
+         */
+        padding_data(view);
+
+        /*
+         * 添加监听
+         */
+        add_listener(view);
+    }
+
+    /**
+     * bind view with layout
+     *
+     * @param view parent
+     */
+    private void bind_view(View view) {
+        et_fund = view.findViewById(R.id.et_fund);
+        et_medical = view.findViewById(R.id.et_medical);
+        et_endowment = view.findViewById(R.id.et_endowment);
+        et_unemployment = view.findViewById(R.id.et_unemployment);
+        et_employment_injury = view.findViewById(R.id.et_employment_injury);
+        et_maternity = view.findViewById(R.id.et_maternity);
+        spinner_city = view.findViewById(R.id.spinner_city);
+        input_salary = view.findViewById(R.id.input_salary);
+        et_threshold = view.findViewById(R.id.input_threshold);
+        compute = view.findViewById(R.id.btn_start_compute2);
         insurance_housing_fund = view.findViewById(R.id.textView23);
         layout = view.findViewById(R.id.ly_insurance_housing_fund);
         layout.setVisibility(View.GONE);
+    }
+
+    /**
+     * padding data from db resource
+     *
+     * @param view parent
+     */
+    private void padding_data(View view) {
+        /*
+         * 在数据库资源中加载数据
+         */
+        final ArrayAdapter<String> city_adapter = new ArrayAdapter<>(view.getContext(), R.layout.item_spinner_select, cityData());
+        city_adapter.setDropDownViewResource(R.layout.item_dialog_spinner_select);
+        spinner_city.setAdapter(city_adapter);
+    }
+
+    /**
+     * edit text event
+     *
+     * @param s        editable
+     * @param df_value default value
+     * @param pointer  mouse pointer
+     * @return real time value
+     */
+    private double et_change(Editable s, double df_value, int pointer, int precision) {
+        double rt_value;
+        String str = s.toString();
+        /*
+         * 若为空，使用默认值
+         */
+        if (str.isEmpty()) {
+            rt_value = df_value;
+        }
+        /*
+         * 若存在小数点
+         */
+        else {
+            if (str.indexOf('.') != -1) {
+                /*
+                 * 最多两位小数，若多于两位小数则无视本次输入
+                 */
+                if (str.indexOf('.') < str.length() - precision - 1) {
+                    s.delete(pointer - 1, pointer);
+                } else
+                    /*
+                     * 若第一个字符为小数点
+                     */
+                    if (str.equals(".")) {
+                        s.insert(0, "0");
+                    }
+            }
+            /*
+             * 0开始的整数
+             */
+            if (str.startsWith("0") && !str.startsWith("0.") && !str.equals("0")) {
+                s.delete(0, 1);
+            }
+            str = s.toString();
+            rt_value = Double.parseDouble(str);
+        }
+        return rt_value;
+    }
+
+    /**
+     * update real time rate
+     */
+    private void update_real_rate() {
+    }
+
+    /**
+     * add listen to view
+     */
+    private void add_listener(final View view) {
+        /*
+         * 五险一金隐藏显示
+         */
         insurance_housing_fund.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -60,43 +171,169 @@ public class monthlySalary extends Fragment implements View.OnClickListener {
             }
         });
 
-        et_fund = view.findViewById(R.id.et_fund);
-        et_medical = view.findViewById(R.id.et_medical);
-        et_endowment = view.findViewById(R.id.et_endowment);
-        et_unemployment = view.findViewById(R.id.et_unemployment);
-        et_employment_injury = view.findViewById(R.id.et_employment_injury);
-        et_maternity = view.findViewById(R.id.et_maternity);
-        spinner_city = view.findViewById(R.id.spinner_city);
-        input_salary = view.findViewById(R.id.input_salary);
-        et_threshold = view.findViewById(R.id.input_threshold);
-        compute = view.findViewById(R.id.btn_start_compute2);
-
-        final ArrayAdapter<String> city_adapter = new ArrayAdapter<>(view.getContext(), R.layout.item_spinner_select, cityData());
-        city_adapter.setDropDownViewResource(R.layout.item_dialog_spinner_select);
-        spinner_city.setAdapter(city_adapter);
-
+        /*
+         * 城市 Spinner 选择事件
+         */
         spinner_city.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                Wuxianyijin wxyj = queryByCity(city_adapter.getItem(position));
-                city = city_adapter.getItem(position);
-                et_fund.setText(new DecimalFormat("#.##").format(wxyj.getPublicfunds() * 100));
-                et_medical.setText(new DecimalFormat("#.##").format(wxyj.getMedicalTreatment() * 100));
-                et_employment_injury.setText(new DecimalFormat("#.##").format(wxyj.getInjury() * 100));
-                et_endowment.setText(new DecimalFormat("#.##").format(wxyj.getOldCare() * 100));
-                et_maternity.setText(new DecimalFormat("#.##").format(wxyj.getProcreation() * 100));
-                et_unemployment.setText(new DecimalFormat("#.##").format(wxyj.getUnempliyed() * 100));
+                String city = String.valueOf(parent.getItemAtPosition(position));
+                wxyj = queryByCity(city);
+                et_fund.setHint(new DecimalFormat("#.##").format(wxyj.getPublicfunds() * 100));
+                et_medical.setHint(new DecimalFormat("#.##").format(wxyj.getMedicalTreatment() * 100));
+                et_employment_injury.setHint(new DecimalFormat("#.##").format(wxyj.getInjury() * 100));
+                et_endowment.setHint(new DecimalFormat("#.##").format(wxyj.getOldCare() * 100));
+                et_maternity.setHint(new DecimalFormat("#.##").format(wxyj.getProcreation() * 100));
+                et_unemployment.setHint(new DecimalFormat("#.##").format(wxyj.getUnempliyed() * 100));
             }
 
             @Override
             public void onNothingSelected(AdapterView<?> parent) {
-                city = city_adapter.getItem(0);
+
             }
         });
 
-        //按钮设置监听
-        compute.setOnClickListener(this);
+        /*
+         * 计算按钮事件
+         */
+        compute.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                btn_click(view);
+            }
+        });
 
+        /*
+         * 文本框编辑事件
+         */
+        input_salary.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                et_change(s, 0, input_salary.getSelectionStart(), 0);
+            }
+        });
+        et_threshold.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                et_change(s, 5000, input_salary.getSelectionStart(), 0);
+            }
+        });
+        et_employment_injury.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                et_change(s, wxyj.getInjury(), et_employment_injury.getSelectionStart(), 0);
+            }
+        });
+        et_unemployment.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                et_change(s, wxyj.getUnempliyed(), et_unemployment.getSelectionStart(), 0);
+            }
+        });
+        et_endowment.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                et_change(s, wxyj.getOldCare(), et_endowment.getSelectionStart(), 0);
+            }
+        });
+        et_medical.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                et_change(s, wxyj.getMedicalTreatment(), et_medical.getSelectionStart(), 0);
+            }
+        });
+        et_fund.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                et_change(s, wxyj.getPublicfunds(), et_fund.getSelectionStart(), 0);
+            }
+        });
+        et_maternity.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                et_change(s, wxyj.getProcreation(), et_maternity.getSelectionStart(), 0);
+            }
+        });
     }
 
     /**
@@ -119,22 +356,46 @@ public class monthlySalary extends Fragment implements View.OnClickListener {
     }
 
 
-    @Override
-    public void onClick(View v) {
+    public void btn_click(View v) {
         try {
-            double threshold;
+            double threshold, fund, medical, endowment, unemployment, maternity, employment_injury;
             double income = Double.parseDouble(input_salary.getText().toString());
             if (et_threshold.getText().toString().equals("")) {
                 threshold = 5000;
             } else {
                 threshold = Double.parseDouble(et_threshold.getText().toString());
             }
-            double fund = Double.parseDouble(et_fund.getText().toString()) / 100;
-            double medical = Double.parseDouble(et_medical.getText().toString()) / 100;
-            double endowment = Double.parseDouble(et_endowment.getText().toString()) / 100;
-            double unemployment = Double.parseDouble(et_unemployment.getText().toString()) / 100;
-            double maternity = Double.parseDouble(et_maternity.getText().toString()) / 100;
-            double employment_injury = Double.parseDouble(et_employment_injury.getText().toString()) / 100;
+            try {
+                fund = Double.parseDouble(et_fund.getText().toString()) / 100;
+            } catch (Exception ex) {
+                fund = wxyj.getPublicfunds();
+            }
+            try {
+                medical = Double.parseDouble(et_medical.getText().toString()) / 100;
+            } catch (Exception ex) {
+                medical = wxyj.getMedicalTreatment();
+            }
+            try {
+                endowment = Double.parseDouble(et_endowment.getText().toString()) / 100;
+            } catch (Exception ex) {
+                endowment = wxyj.getOldCare();
+            }
+            try {
+                unemployment = Double.parseDouble(et_unemployment.getText().toString()) / 100;
+            } catch (Exception ex) {
+                unemployment = wxyj.getUnempliyed();
+            }
+            try {
+                maternity = Double.parseDouble(et_maternity.getText().toString()) / 100;
+            } catch (Exception ex) {
+                maternity = wxyj.getProcreation();
+            }
+            try {
+                employment_injury = Double.parseDouble(et_employment_injury.getText().toString()) / 100;
+            } catch (Exception ex) {
+                employment_injury = wxyj.getInjury();
+            }
+
 
             Intent intent = new Intent(getActivity(), PersonActivity.class);
             Bundle bundle = new Bundle();
@@ -147,8 +408,7 @@ public class monthlySalary extends Fragment implements View.OnClickListener {
             bundle.putDouble("unemployment", unemployment);
             bundle.putDouble("maternity", maternity);
             bundle.putDouble("employment_injury", employment_injury);
-            bundle.putString("city", city);
-
+            bundle.putString("city", String.valueOf(spinner_city.getSelectedItem()));
             intent.putExtras(bundle);
             startActivity(intent);
         } catch (Exception ex) {
